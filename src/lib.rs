1223ab2c9efca12ea41233cd525a224706a02d95
Daniel Abramov <dabramov@snapview.de>
2018-01-21 22:10:14 +0100

    Bump gtk-sys version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+
 extern crate gtk_sys;
-use gtk_sys::{GtkStatusIconPrivate, GtkStatusIcon, GtkMenu, GtkWidget};
+
+use gtk_sys::{GtkStatusIcon, GtkMenu, GtkWidget};
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
